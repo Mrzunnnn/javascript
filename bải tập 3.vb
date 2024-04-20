05ed9b8f3329dedf76b0b48bb82fabd6cf79b6d1
Mrzunnnn <148939431+Mrzunnnn@users.noreply.github.com>
2024-04-20 17:35:27 +0700

    .

---
+++ bải tập 3.vb
@@ -0,0 +1,60 @@
+
+câu 1
+// const p1 = document.querySelector("#text");
+
+// p1.classList.add("text-h1");
+
+// p1.textContent = "Tôi có thể làm <em> bất cứ điều gì </em> tôi muốn với JavaScript."
+
+
+câu 2
+
+// const l1 = document.querySelectorAll("#list")
+
+// l1.forEach(function(item) {
+//     item.style.color = "blue";
+// });
+
+
+câu 3
+
+// const mylist = document.querySelector("#list")
+
+// const newli8 = document.createElement("li")
+
+// newli8.textContent = "item 8"
+
+// const newli9 = document.createElement("li")
+
+// newli9.textContent = "item 9"
+
+// const newli10 = document.createElement("li")
+
+// newli10.textContent = "item 10"
+
+
+
+// mylist.appendChild(newli8);
+// mylist.appendChild(newli9);
+// mylist.appendChild(newli10);
+
+
+// const li1 = mylist.children[0]
+
+// li1.style.color = "red";
+
+// const li3 = mylist.children[2]
+
+// li3.style.color = "blue"
+
+
+// const li4 = mylist.children[3]
+
+// mylist.removeChild(li4)
+
+
+// const newli = document.createElement("li")
+
+// newli.textContent = "hello new li"
+
+// li3.append(newli)
